5ad406035fb8154a9a0a7073cafe225db28cd92e
Luana Paes <luanafranca598@gmail.com>
2023-08-03 20:42:13 -0300

    changes

--- ecommerce-kalyne/js/carregar.js
+++ ecommerce-kalyne/js/carregar.js
@@ -18,6 +18,33 @@ uploadInput.addEventListener('change', function () {
 function formatarMoeda(input) {
     let valor = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
     valor = (valor / 100).toFixed(2); // Divide por 100 e formata para 2 casas decimais
-  
+
     input.value = `${valor.replace(".", ",")}`; // Adiciona "R$" e substitui o ponto por vírgula
-  }
+}
+
+function cadastrarProduto() {
+    const marcaSelecionada = document.getElementById('select').value;
+    const nomeProduto = document.getElementById('nome-produto').value;
+    const valorProduto = document.getElementById('valor-produto').value;
+    const valorParcelado = document.getElementById('valor-parcelado').value;
+    const imagemProduto = document.getElementById('uploaded-image').src;
+
+    // Criação do novo card com os dados do produto cadastrado
+    const novoCard = document.createElement('div');
+    novoCard.classList.add('card');
+    novoCard.innerHTML = `
+        <img src="${imagemProduto}" alt="">
+        <h2 class="titleCard">${nomeProduto}</h2>
+        <p id="preco">R$${valorProduto}</p>
+        <i>ou ${valorParcelado}</i>
+        <button class="buy-button" data-produto="${nomeProduto}">Clique para comprar</button>
+    `;
+
+    // Insere o novo card na div correspondente à marca selecionada
+    const divMarca = document.getElementById(`marca-${marcaSelecionada.toLowerCase()}`);
+    if (divMarca) {
+        divMarca.appendChild(novoCard);
+    }
+}
+
+document.querySelector('.btn-card').addEventListener('click', cadastrarProduto());
