667f19affdb926cd56301ee96d979331f8501106
Luana Paes <luanafranca598@gmail.com>
2023-08-03 00:06:20 -0300

    chances
    tentando redirecionar botão para o wpp com mensagem

--- ecommerce-kalyne/js/script.js
+++ ecommerce-kalyne/js/script.js
@@ -1,26 +1,36 @@
-// Seleciona todos os botões com a classe .buy-button
-const buyButtons = document.querySelectorAll('.buy-button');
 
-// Adiciona um ouvinte de evento de clique para cada botão
-buyButtons.forEach(button => {
-  button.addEventListener('click', function() {
-    // Função a ser executada quando um botão for clicado
-    const productName = this.parentElement.querySelector('h2').innerText;
-    alert('Você clicou no botão de comprar do produto: ' + productName);
-  });
-});
-
-function voltar(){
+function voltar() {
   window.location = 'index.html';
 }
 
 
-document.getElementById('link-transicao').addEventListener('click', function(event) {
+document.getElementById('link-transicao').addEventListener('click', function (event) {
   event.preventDefault();
-  
-  const targetId = this.getAttribute('href').substring(1); 
+
+  const targetId = this.getAttribute('href').substring(1);
 
   const targetElement = document.getElementById(targetId);
 
   targetElement.scrollIntoView({ behavior: 'smooth' });
 });
+
+
+const buyButtons = document.querySelectorAll(".buy-button");
+
+function handleBuyButtonClick(event) {
+  const produto = event.target.getAttribute("data-produto");
+
+  const mensagem = `Quero o produto "${produto}". Como faço para adquiri-lo?`;
+
+  const numeroWhatsApp = "558198661064";
+
+  const urlWhatsApp = `https://api.whatsapp.com/send?phone=&text=${encodeURIComponent(mensagem)}`;
+
+  window.location.href = urlWhatsApp;
+}
+
+buyButtons.forEach(button => {
+  button.addEventListener("click", handleBuyButtonClick);
+});
+
+
