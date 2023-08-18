27683c1ced5790b8f078dac850f438230b06a5fe
Luana Paes <luanafranca598@gmail.com>
2023-08-18 14:37:32 -0300

    add

--- ecommerce-kalyne/js/carregar.js
+++ ecommerce-kalyne/js/carregar.js
@@ -2,58 +2,69 @@ const uploadInput = document.getElementById('upload-input');
 const uploadedImage = document.getElementById('uploaded-image');
 
 uploadInput.addEventListener('change', function () {
-    const file = uploadInput.files[0];
+  const file = uploadInput.files[0];
 
-    if (file) {
-        const reader = new FileReader();
+  if (file) {
+    const reader = new FileReader();
 
-        reader.onload = function () {
-            uploadedImage.src = reader.result;
-        };
+    reader.onload = function () {
+      uploadedImage.src = reader.result;
+    };
 
-        reader.readAsDataURL(file);
-    }
+    reader.readAsDataURL(file);
+  }
 });
 
 function formatarMoeda(input) {
-    let valor = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
-    valor = (valor / 100).toFixed(2); // Divide por 100 e formata para 2 casas decimais
+  let valor = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+  valor = (valor / 100).toFixed(2); // Divide por 100 e formata para 2 casas decimais
 
-    input.value = `${valor.replace(".", ",")}`; // Adiciona "R$" e substitui o ponto por vírgula
+  input.value = `${valor.replace(".", ",")}`; // Adiciona "R$" e substitui o ponto por vírgula
 }
 
 function cadastrarProduto() {
-    const marcaSelecionada = document.getElementById('select').value;
-    const nomeProduto = document.getElementById('nome-produto').value;
-    const valorProduto = document.getElementById('valor-produto').value;
-    const valorParcelado = document.getElementById('valor-parcelado').value;
-    const imagemProduto = document.getElementById('uploaded-image').src;
-  
-    // cria o card com as informações do produto cadastrado
-    const novoCard = document.createElement('div');
-    novoCard.classList.add('card');
-    novoCard.innerHTML = `
-      <img src="${imagemProduto}" alt="">
-      <h2 class="titleCard">${nomeProduto}</h2>
-      <p id="preco">R$${valorProduto}</p>
+  alert('foi clicado');
+
+  var Produtos = (function (window) {
+    'use strict'
+    function _produto(marca, nome, valor, valorparcelado, imagem) {
+      this.marca = document.getElementById('#select').value;
+      this.nome = document.getElementById('#nome-produto').value;
+      this.valor = document.getElementById('#valor-produto').value;
+      this.valorParcelado = document.getElementById('#valor-parcelado').value;
+      this.imagem = document.getElementById('#uploaded-image').src;
+
+      // cria o card com as informações do produto cadastrado
+      const novoCard = document.createElement('div');
+      novoCard.classList.add('card');
+      novoCard.innerHTML = `
+      <img src="${imagem}" alt="">
+      <h2 class="titleCard">${nome}</h2>
+      <p id="preco">R$${valor}</p>
       <i>ou ${valorParcelado}</i>
-      <button class="buy-button" data-produto="${nomeProduto}">Clique para comprar</button>
+      <button class="buy-button" data-produto="${nome}">Clique para comprar</button>
     `;
-  
-    const divMarca = document.querySelector(`[data-marca="${marcaSelecionada}"]`);
-    if (divMarca) {
-      divMarca.appendChild(novoCard);
+
+      const divMarca = document.querySelector(`[data-marca="${marcaSelecionada}"]`);
+      if (divMarca) {
+        divMarca.appendChild(novoCard);
+      }
+
+      // lista com os cards
+      const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+
+      // Adicionar o novo card à lista 
+      const newTaskText = `${nome} ${valor} ${valorParcelado} ${imagem}`;
+      taskList.push(newTaskText);
+      localStorage.setItem('tasks', JSON.stringify(taskList));
     }
+
+    // Adicione o event listener para a função cadastrarProduto
+    document.querySelector('.btn-cadastrar').addEventListener('click', cadastrarProduto);
+    return _produto;
+  })(window);
   
-    // lista com os cards
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-  
-    // Adicionar o novo card à lista 
-    const newTaskText = `${nomeProduto} ${valorProduto} ${valorParcelado}`;
-    taskList.push(newTaskText);
-    localStorage.setItem('tasks', JSON.stringify(taskList));
-  }
-  
-  // Adicione o event listener para a função cadastrarProduto
-  document.querySelector('.btn-cadastrar').addEventListener('click', cadastrarProduto);
-  
+}
+
+
+
--- ecommerce-kalyne/js/script.js
+++ ecommerce-kalyne/js/script.js
@@ -30,35 +30,13 @@ function handleBuyButtonClick(event) {
 }
 
 
-buyButtons.forEach(button => {
-  button.addEventListener("click", handleBuyButtonClick);
-});
 
-String.prototype.reverse = function () {
-  return this.split('').reverse().join('');
-};
-
-function mascaraMoeda(campo, evento) {
-  var tecla = (!evento) ? window.event.keyCode : evento.which;
-  var valor = campo.value.replace(/[^\d]+/gi, '').reverse();
-  var resultado = "";
-  var mascara = "##.###.###,##".reverse();
-  for (var x = 0, y = 0; x < mascara.length && y < valor.length;) {
-    if (mascara.charAt(x) != '#') {
-      resultado += mascara.charAt(x);
-      x++;
-    } else {
-      resultado += valor.charAt(y);
-      y++;
-      x++;
-    }
-  }
-  campo.value = resultado.reverse();
+function entrar() {
+  window.location = 'login.html';
 }
 
-
-function entrar(){
-  window.location = 'login.html';
+function telaCadastrarProdutos() {
+  window.location = '../index.html';
 }
 
 
